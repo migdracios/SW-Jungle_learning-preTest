7b26c28115dda71126abe592cc28497332c4dd01
migdracios <migdracios@gmail.com>
2022-02-02 11:12:55 +0900

    0201 2022 learning

---
+++ chapter 03/hello.py
@@ -0,0 +1,32 @@
+# print('Hello, Jungle')
+
+# a=3
+# print(a)
+# b=a
+# print(b)
+# a=5
+# print(a,b)
+
+# a=3
+# a
+
+# a = 4.8
+
+# a = 7
+# b = 2
+
+# a+b     # 9
+# a-b     # 5
+# a*b     # 14
+# a/b     # 3.5
+# a//b    # 3 (몫)
+# a%b     # 1 (나머지)
+# a**b    # 49 (거듭제곱)
+
+# a+3*b   # 13 (사칙연산 순서대로)
+
+a = 5
+a = a + 3   # 5에 3을 더한 값이 a에 저장
+print(a)
+a+=1
+print(a)
